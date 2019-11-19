80b5250098d42236f99e1452d5a9c15c8fa1bd21
Your Name <you@example.com>
2019-11-19 19:01:56 +0530

    commit on 19-11-19

--- DS/Mergesort.java
+++ DS/Mergesort.java
@@ -2,21 +2,20 @@ package DS;
 
 public class Mergesort 
 {
-	private static int m;
 	public static void mergesort(int a[],int l,int h)
 	{
-		if(l<h)
-		{
+		if(l<h) {
 			int m=(l+h)/2;
 			mergesort(a,l,m);
 			mergesort(a,m+1,h);
 			merge(a,l,m,h);
 		}
-	}
-	public static void merge(int a[],int l,int h,int m)
+		
+		}
+	public static void merge(int a[],int l,int m,int h)
 	{
 		int b[]=new int[h-l+1];
-		int i=1,k=0;
+		int i=l,k=0;
 		int j=m+1;
 		while(i<=m && j<=h)
 		{
@@ -24,7 +23,7 @@ public class Mergesort
 				b[k++]=a[i++];
 			else
 				b[k++]=a[j++];
-	     }
+		}
 		while(i<=m)
 		{
 			b[k++]=a[i++];
@@ -36,14 +35,27 @@ public class Mergesort
 		for(k=0;k<b.length;k++)
 		{
 			a[l+k]=b[k];
+					
 		}
 	}
+	
+	public static void printarray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i=0; i<n; ++i) 
+            System.out.print(arr[i] + " "); 
+        System.out.println(); 
+    } 
+	 
 public static void main(String[] args) 
 {
 	int a[]= {38,27,43,3,9,82,10};
-	int l=a[0];
-	int h=a.length-1;
-	mergesort(a,l,h);
-	merge(a, l, h, m);
+	
+	Mergesort ob=new Mergesort();
+	ob.mergesort(a,0,a.length-1);
+	
+	System.out.println("\nSorted array"); 
+    printarray(a); 
+    System.out.println("----");
 }
 }
